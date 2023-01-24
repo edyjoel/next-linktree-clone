63df17be4ffcf612b1a0fb0e535d9506c14d183e
Edy <edyxicon@gmail.com>
2023-01-23 21:24:38 -0600

    Git init

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Linktree</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="Next.js + Tailwind CSS + TypeScript template"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
